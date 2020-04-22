515e28af3c84bd0371894e208963022d6724172f
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-04-22 22:11:37 +0200

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container fle bg-black min-h-screen min-w-full">
+      <header className="pt-16">
+        <img src={logo} className="rounded-sm h-24 mx-auto" alt="logo" />
       </header>
     </div>
   );
