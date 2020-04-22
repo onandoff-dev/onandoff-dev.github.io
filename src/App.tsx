4c99fbe5d44d12d7ff477995df10285cd21a3f1f
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-04-23 00:44:03 +0200

    Init page CI

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import logo from './logo.svg';
 import './styles/tailwind.css';
 
 function App() {
-  return (
-    <div className="container fle bg-black min-h-screen min-w-full">
-      <header className="pt-16">
-        <img src={logo} className="rounded-sm h-24 mx-auto" alt="logo" />
-      </header>
-    </div>
-  );
+    return (
+        <div className="mx-auto flex flex-col bg-white min-h-screen">
+            <main className="onandoff-hero-bg flex-grow">
+            </main>
+            <footer className="flex flex-row bg-black w-full h-24">
+                <div className="flex flex-row container items-center justify-start mx-auto">
+                    <img src={logo} className="rounded-sm ml-6 h-8" alt="logo"/>
+                    <p>
+                        <a className="text-white ml-2 text-base font-normal font-sans leading-loose tracking-tight"
+                           href="https://github.com/onandoff-dev/">onandoff.io</a>
+                    </p>
+                </div>
+            </footer>
+
+        </div>
+    );
 }
 
 export default App;
