2103cf6d97af70cba184dd9d6ac2bb72e8bc0377
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-04-22 21:30:18 +0200

    Updates

---
+++ precache-manifest.c18167d4143550621b066374211ed14a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d48920c732d296b832e1559ea67552f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2242a802ed166bd62cb1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52035f85d9f03fdddb9c",
+    "url": "/static/js/2.320bf351.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.320bf351.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2242a802ed166bd62cb1",
+    "url": "/static/js/main.d4a0f8c7.chunk.js"
+  },
+  {
+    "revision": "51189366d32a40a7c8bb",
+    "url": "/static/js/runtime-main.001d9ceb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
